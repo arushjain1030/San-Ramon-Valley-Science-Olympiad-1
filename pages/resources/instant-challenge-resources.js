022dde7f832f55f0a21920f8e20c90f8f28d696e
Anthony Kuang <kuanganthony@gmail.com>
2022-02-09 18:49:47 -0800

    Instant challenge

--- pages/resources/instant-challenge-resources.js
+++ pages/resources/instant-challenge-resources.js
@@ -13,15 +13,57 @@ export default function InstantChallengeResources() {
           SRVSO).
         </p>
       </div>
-      <div className="flex flex-col w-full overflow-hidden h-96">
-        <iframe
-          title="SRVSO Presentation"
-          src="https://docs.google.com/presentation/d/e/2PACX-1vSaO1EsfG4m2G92mzTrgvUBZ4_S7Wqtx1XCwwp7ndQjm01KxH5J6ASryEF79r1urQhTLk5_j06HesVv/embed"
-          allowFullScreen="true"
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-          className="flex-grow"
-        />
+
+      <div
+        className="px-8 my-8 border border-gray-800 py-9"
+        id="100th-floor-cathedral"
+      >
+        <div className="flex items-center mb-6 gap-x-2">
+          <div className="grid w-8 h-8 bg-gray-700 rounded-full place-items-center">
+            <span className="text-lg font-medium text-gray-100">1</span>
+          </div>
+          <p className="text-gray-400">100th Floor Cathedral</p>
+        </div>
+        <p className="px-12">
+          * This is just a possible example of an instant challenge that will be
+          administered on the day of the event.
+        </p>
+        <div className="flex flex-col w-full mt-8 overflow-hidden h-96">
+          <iframe
+            title="100th Floor Cathedral"
+            src="https://docs.google.com/document/d/e/2PACX-1vTzQI0xlbMn9m5siSqO-4jZGCsRN9h0Yfuileeb5e76Ar7wu6gfmfZ1BGeqZ5_NqPHrbu9qpG1TE1CX/pub?embedded=true"
+            className="flex-grow"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </div>
+
+      <div
+        className="px-8 my-8 border border-gray-800 py-9"
+        id="Slide for Life"
+      >
+        <div className="flex items-center mb-6 gap-x-2">
+          <div className="grid w-8 h-8 bg-gray-700 rounded-full place-items-center">
+            <span className="text-lg font-medium text-gray-100">2</span>
+          </div>
+          <p className="text-gray-400">Slide for Life</p>
+        </div>
+        <p className="px-12">
+          * This is just a possible example of an instant challenge that will be
+          administered on the day of the event.
+        </p>
+        <div className="flex flex-col w-full mt-8 overflow-hidden h-96">
+          <iframe
+            title="Slide for Life"
+            src="https://docs.google.com/document/d/e/2PACX-1vRfCtvQd0WUkWheMc4RwqFqMiJpLQath5KRidCRs4g4pOVKnF08Wy9bHRoxixQathC1zvG1j-1CCOde/pub?embedded=true"
+            className="flex-grow"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
       </div>
     </>
   );
