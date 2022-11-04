3611fa7036c542aad01e9befe0b2840206133366
Fatimah Hussain <fatimahandhussain@gmail.com>
2022-11-03 21:43:12 -0700

    hp

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
       <a>
         <h2 className="mt-6 mb-4 text-2xl text-center">
           {' '}
-          Registration is now closed. Please look out for SRVSO 2023!
+          Registration is now closed. Please look out for SRVSO 2023-2024!
         </h2>
         {/* <Button className="mx-auto mt-20 text-lg">
             Register for The SRVSO 2023 Now!!
