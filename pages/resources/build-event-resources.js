12fe3d2c31b84bec5b35b018684422478764390d
Fatimah Hussain <67985868+anony-mouse1@users.noreply.github.com>
2023-01-25 13:41:11 -0800

    Update build-event-resources.js

--- pages/resources/build-event-resources.js
+++ pages/resources/build-event-resources.js
@@ -1,40 +1,41 @@
+import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
+import Carousel from 'nuka-carousel';
+import { RiErrorWarningLine } from 'react-icons/ri';
 
 export default function BuildEventResources() {
   return (
     <>
       <Head>
-        <title>Build Event Resources — SRVSO</title>
+        <title> Build Event Resources — SRVSO</title>
         <meta
           name="description"
-          content="More details for each SRVSO 2022 build event."
+          content="Here's where you can find all the relevant resources for SRVSO 2022-23"
         />
       </Head>
+
       <div className="mb-12">
-        <h2 className="mb-4">Build Event Resources</h2>
-        <p className="text-lg">More details for each SRVSO 2022 build event.</p>
+        <h2 className="mb-4">Resources</h2>
+        <p className="text-lg">
+          Here's where you can find all the build event resources for SRVSO
+          2022-23
+        </p>
       </div>
 
-      <div className="px-8 my-8 border border-gray-800 py-9">
-        <div className="flex items-center mb-6 gap-x-2">
-          <div className="grid w-8 h-8 bg-gray-700 rounded-full place-items-center">
-            <span className="pb-0.5 text-lg font-medium text-gray-100">
-              &#10031;
-            </span>
-          </div>
-          <p className="text-gray-400">Build Event Descriptions</p>
-        </div>
+      <div className="mb-12">
+        <h3 className="mt-6 text-lg">Build Event Procedures</h3>
         <div className="flex flex-col w-full mt-8 overflow-hidden h-96">
           <iframe
-            title="Build Event Descriptions"
-            src="https://docs.google.com/document/d/1m2xZoGhsJLra8yT-IC5vEmFG589PcHvxKQobaRe4EB8"
+            title="Build Event Procedures"
+            src="https://docs.google.com/document/d/1lQpYZ7lNUq0EOwuhcM06zo73lumJ5suv4ty1K6Uwjn4/edit"
             className="flex-grow"
             frameBorder="0"
             allow="autoplay; fullscreen; picture-in-picture"
             allowFullScreen
           />
           <a
-            href="https://docs.google.com/document/d/1m2xZoGhsJLra8yT-IC5vEmFG589PcHvxKQobaRe4EB8"
+            href="https://docs.google.com/document/d/1lQpYZ7lNUq0EOwuhcM06zo73lumJ5suv4ty1K6Uwjn4/edit"
             target="_blank"
             className="mt-2 text-sm text-center underline"
             rel="noopener noreferrer"
@@ -42,6 +43,51 @@ export default function BuildEventResources() {
             External link
           </a>
         </div>
+
+        <h3 className="mt-6 text-lg">100th Floor Cathedral</h3>
+        <div className="flex flex-col w-full mt-8 overflow-hidden h-96">
+          <iframe
+            title="100th Floor Cathedral"
+            src="https://docs.google.com/document/d/1Pf-RAQqcGTdDNEYSTXhaB-q5rDVVDj2fcLgeiMGsx5E/edit"
+            className="flex-grow"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+          />
+          <a
+            href="https://docs.google.com/document/d/1Pf-RAQqcGTdDNEYSTXhaB-q5rDVVDj2fcLgeiMGsx5E/edit"
+            target="_blank"
+            className="mt-2 text-sm text-center underline"
+            rel="noopener noreferrer"
+          >
+            External link
+          </a>
+        </div>
+
+        <h3 className="mt-6 text-lg">Build Binder Layout</h3>
+        <div className="flex flex-col w-full mt-2 overflow-hidden h-96">
+          <iframe
+            title="Results"
+            src="https://docs.google.com/document/d/10cD0wL1Ghy0SRou4Y7jIUhFyDVVq7-d9W4ytDCK_h0k/edit?usp=sharing"
+            className="flex-grow"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+          />
+          <a
+            href="https://docs.google.com/document/d/10cD0wL1Ghy0SRou4Y7jIUhFyDVVq7-d9W4ytDCK_h0k/edit?usp=sharing"
+            target="_blank"
+            className="mt-2 text-sm text-center underline"
+            rel="noopener noreferrer"
+          >
+            External link
+          </a>
+        </div>
+        {/* <h2 className="mb-4">2022 Olympiad</h2>
+        <p className="mb-4 text-lg">
+          The 4th annual SRVSO was held at Windemere Ranch Middle School in
+          April 2022.
+        </p> */}
       </div>
     </>
   );
