a9c730fef7040976abc570086faf08f207df478e
arushjain1030 <133838590+arushjain1030@users.noreply.github.com>
2023-09-16 09:04:05 -0700

    Updated Board of Officers

--- pages/board-of-officers.js
+++ pages/board-of-officers.js
@@ -23,7 +23,7 @@ export default function BoardOfOfficers() {
         <p className="text-lg">The board that makes SRVSO possible.</p>
       </div>
       <div className="space-y-5">
-        <div className="p-2 text-white rounded bg-lime-800">
+        {/* <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
             <b>Alagu Subramanian</b>
@@ -41,51 +41,86 @@ export default function BoardOfOfficers() {
             been with the SRVSO since its beginning as our founder and currently
             serves as the Chairman of our Board of Directors.{' '}
           </p>
-        </div>
+        </div> */}
 
         <h2 className="pt-10"> Executive Sector </h2>
 
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
-            <b>Arav Bhattacharya</b>
-          </h3>
-          <p>Executive Director</p>
-        </div>
-
-        <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/arav.jpg" />
-          <p className="p-5">
-            {' '}
-            Arav is a senior at Dougherty Valley High School. An aspiring
-            mathematician, he is interested in enriching the quality of STEM
-            education in hopes of helping students find the beauty in these
-            subjects. Arav has been with SRVSO for 4 years and currently serves
-            as the Executive Director.{' '}
-          </p>
-        </div>
-
-        <div className="p-2 text-white rounded bg-lime-800">
-          <h3>
-            {' '}
             <b>Sundar Subramanian</b>
           </h3>
-          <p>Associate Director</p>
+          <p>Executive Director</p>
         </div>
 
         <div className="flex items-center text-white">
           <img className="h-40 mt-4" src="/images/officers/sundar.png" />
           <p className="p-5">
             {' '}
-            Sundar is a sophomore at Dougherty Valley High School. He is
-            interested in the Artificial Intelligence field with a passion for
-            giving back to the community, and providing the same opportunities
-            he was given as a child to today's youth. Sundar has been with the
-            SRVSO for 5 years and currently serves as the Associate Director.{' '}
+            Sundar is a junior at Dougherty Valley High School. He is interested 
+            in the Artificial Intelligence field with a passion for giving back 
+            to the community, and providing the same opportunities he was given 
+            as a child to today's youth. Sundar has been with the SRVSO for 6 years
+            and currently serves as the Executive Director.{' '}
           </p>
         </div>
 
-        <h2 className="pt-10"> Operational Sector </h2>
+        <div className="p-2 text-white rounded bg-lime-800">
+          <h3>
+            {' '}
+            <b>Arush Jain</b>
+          </h3>
+          <p>Associate Director</p>
+        </div>
+
+        <div className="flex items-center text-white">
+          <img className="h-40 mt-4" src="/images/officers/arush.jpg" />
+          <p className="p-5">
+            {' '}
+            Arush is a junior at Dougherty Valley High School. He is an aspiring software
+            engineer with a passion for innovative technology. Arush has been with SRVSO
+            for 3 years and currently serves as an Associate Director.{' '}
+          </p>
+        </div>
+
+        <div className="p-2 text-white rounded bg-lime-800">
+          <h3>
+            {' '}
+            <b>Nikitha Bangalore</b>
+          </h3>
+          <p>Associate Director</p>
+        </div>
+
+        <div className="flex items-center text-white">
+          <img className="h-40 mt-4" src="/images/officers/nikitha.png" />
+          <p className="p-5">
+            {' '}
+            Nikitha is a senior at Dougherty Valley High School. She is currently exploring 
+            her options for possible careers, but is passionate about giving younger students
+            exposure to STEM related activities. Nikitha has been with SRVSO for 2 years and
+            currently serves as the Associate Director.{' '}
+          </p>
+        </div>
+
+        <div className="p-2 text-white rounded bg-lime-800">
+          <h3>
+            {' '}
+            <b>Ansh Gandhi</b>
+          </h3>
+          <p>Logistics Head</p>
+        </div>
+
+        <div className="flex items-center text-white">
+          <img className="h-40 mt-4" src="/images/officers/ansh.png" />
+          <p className="p-5">
+            {' '}
+            Ansh is a senior at Dougherty Valley High School. An aspiring entrepreneur, 
+            he is interested in helping show students the beauty of science. Ansh has 
+            been with SRVSO for 2 years and currently serves as the Logistics Head{' '}
+          </p>
+        </div>
+
+        {/* <h2 className="pt-10"> Operational Sector </h2>
 
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
@@ -145,137 +180,116 @@ export default function BoardOfOfficers() {
             careers in the CS field. Pranav has been with SRVSO for 3 years and
             is the Logistics Director.
           </p>
-        </div>
+        </div> */}
         <h2 className="pt-10"> Build Events Committee </h2>
-
-        <div className="p-2 text-white rounded bg-lime-800">
-          <h3>
-            {' '}
-            <b>Praveer Balaka</b>
-          </h3>
-          <p>Build Events Head</p>
-        </div>
-
-        <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/praveer.jpg" />
-          <p className="p-5">
-            Praveer is a senior at Dougherty Valley High School. He is an
-            aspiring neurosurgeon, and has a passion for introducing the younger
-            generation to the world of STEM. Praveer is the current Director of
-            Build Events, and this is his second year working with SRVSO.
-          </p>
-        </div>
-
-        <div className="p-2 text-white rounded bg-lime-800">
-          <h3>
-            {' '}
-            <b>Ayan Bhatia</b>
-          </h3>
-          <p>Event Officer</p>
-        </div>
-
-        <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/ayan.jpg" />
-          <p className="p-5">
-            Ayan is a senior at Dougherty Valley High School. He is an aspiring
-            engineering who loves to inspire a love of science in children at a
-            young age. Ayan is currently on the Build Event Board and has been
-            an SRVSO officer for one year.
-          </p>
-        </div>
-
-        <div className="p-2 text-white rounded bg-lime-800">
-          <h3>
-            {' '}
-            <b>Ansh Gandhi</b>
-          </h3>
-          <p>Event Officer</p>
-        </div>
-
-        <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/ansh.png" />
-          <p className="p-5">
-            Ansh is a Junior at Dougherty Valley High School. He is an aspiring
-            entrepreneur with a passion for business and improving the world.
-            Ansh has been with SRVSO for a year and currently serves as a Build
-            Team Officer.
-          </p>
-        </div>
 
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
             <b>Auron Bhattacharya</b>
           </h3>
+          <p>Build Events Head</p>
+        </div>
+
+        <div className="flex items-center text-white">
+          <img className="h-40 mt-4" src="/images/officers/auron.png" />
+          <p className="p-5">
+          Auron is a freshman at Dougherty Valley High School. He is excited for his
+          second year as a member of the SRVSO and he joined it to provide his love
+          of science to the kids of the community. He is currently the Build Events Head.
+          </p>
+        </div>
+
+        <div className="p-2 text-white rounded bg-lime-800">
+          <h3>
+            {' '}
+            <b>Vedanth Dala</b>
+          </h3>
           <p>Event Officer</p>
         </div>
 
         <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/auron.png" />
+          <img className="h-40 mt-4" src="/images/officers/vedanth.png" />
           <p className="p-5">
-            Auron is a first year member of the SRVSO in the build committee.
-            Auron is excited to be working with the rest of the Science Olympiad
-            team to get elementary-age kids interested in different fields of
-            science.
+          Vedanth is a sophomore at Dougherty Valley High School. He aspires to be a
+          physicist, and he loves spreading his passion for science to younger students.
+          This is Vedanth's first year at SRVSO, where he is currently a member of the 
+          Build Event Board.
           </p>
         </div>
+
+        <div className="p-2 text-white rounded bg-lime-800">
+          <h3>
+            {' '}
+            <b>Neha Subramani</b>
+          </h3>
+          <p>Event Officer</p>
+        </div>
+
+        <div className="flex items-center text-white">
+          <img className="h-40 mt-4" src="/images/officers/neha.png" />
+          <p className="p-5">
+          Neha is a sophomore at Dougherty Valley High School. She plans to work as a
+          software engineer in the future and is excited to help SRVSO provide enriching
+          STEM opportunities to kids. Neha is currently a Build Event Officer, and this 
+          is her first year with SRVSO.
+          </p>
+        </div>
+
+        
 
         <h2 className="pt-10"> Theory Events Committee </h2>
 
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
-            <b>Akshay Kannan</b>
+            <b>Megha Guntuku</b>
           </h3>
           <p>Theory Events Head</p>
         </div>
 
         <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/akshay.jpg" />
+          <img className="h-40 mt-4" src="/images/officers/megha.png" />
           <p className="p-5">
-            Akshay Kannan is a senior at Dougherty Valley High School. He is an
-            aspiring computer programmer with a passion in Artificial
-            Intelligence. He has been a part of SRVSO and joined it to provide
-            his love of science to the kids of his community. He is currently
-            the director of theory events
+          Megha is a senior at Dougherty Valley High School and hopes to work in
+          the dental field in the future. She is passionate about preparing young
+          students for higher level STEM classes. This is her second year with SRVSO,
+          and she currently serves as the Theory Head.
           </p>
         </div>
 
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
-            <b>Megha Guntuku</b>
+            <b>Supreetha Jammalamadaka</b>
           </h3>
           <p>Event Officer</p>
         </div>
 
         <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/megha.jpg" />
+          <img className="h-40 mt-4" src="/images/officers/supreetha.png" />
           <p className="p-5">
-            Megha is currently a junior at Dougherty Valley High School and
-            hopes to work in the medical field in the future. She enjoys being a
-            part of the SRVSO team as it allows her to take on an active role in
-            preparing elementary school students for higher-level science
-            classes. This is her first year at SRVSO, and she currently serves
-            as a Theory Officer.
+          Supreetha is a Junior at Dougherty Valley High School who is working towards
+          going into the Computer Science field. She is excited to help young kids 
+          discover their passion, and hopes to inspire their future works. This is
+          her first year at SRVSO, and her current position is Theory Officer.
           </p>
         </div>
 
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
-            <b>Arush Jain</b>
+            <b>Sanjana Rao</b>
           </h3>
           <p>Event Officer</p>
         </div>
 
         <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/arush.jpg" />
+          <img className="h-40 mt-4" src="/images/officers/sanjana.png" />
           <p className="p-5">
-            Arush is a sophomore at Dougherty Valley High School. He is an
-            aspiring software engineer with a passion for innovative technology.
-            Arush has been with SRVSO for 1 year and currently serves as a
-            Theory Event Officer.
+          Sanjana is a senior at Dougherty Valley High School. She is passionate about
+          Math and Biology and hopes to pursue a career in research. This is her
+          first year with SRVSO and she currently serves as a Theory Event Officer.
           </p>
         </div>
 
@@ -284,36 +298,34 @@ export default function BoardOfOfficers() {
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
-            <b>Nikitha Bangalore</b>
+            <b>Chhavi Bidhuri</b>
           </h3>
           <p>Instant Challenge Officer</p>
         </div>
 
         <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/nikitha.jpg" />
+          <img className="h-40 mt-4" src="/images/officers/chhavi.png" />
           <p className="p-5">
-            Nikitha is a junior at Dougherty Valley High School. She is
-            currently exploring her options for possible careers, but is
-            passionate about giving younger students exposure to STEM related
-            activities. Nikitha has been with SRVSO for 1 year and currently
-            serves as the Event Head of Instant Challenge.
+          Chhavi is a sophomore at Dougherty Valley High School.
+          An aspiring marine biologist, her goal is to help support the spread
+          of careers in STEM for young, aspiring students. This is Chhavi's
+          first year with SRVSO and she currently is an Instant Challenge
+          officer.
+
           </p>
         </div>
         <div className="p-2 text-white rounded bg-lime-800">
           <h3>
             {' '}
-            <b>Adviti Aleti</b>
+            <b>Caden Chang</b>
           </h3>
           <p>Instant Challenge Officer</p>
         </div>
 
         <div className="flex items-center text-white">
-          <img className="h-40 mt-4" src="/images/officers/adviti.jpg" />
+          <img className="h-40 mt-4" src="/images/officers/noPicture.png" />
           <p className="p-5">
-            Adviti Aleti is a junior. She is an aspiring medical researcher. Her
-            passion is making STEM learning engaging and accessible to young
-            students. Adviti has been with SRVSO for two years and currently
-            serves as Event Head of Instant Challenge
+            Caden is ...
           </p>
         </div>
       </div>
