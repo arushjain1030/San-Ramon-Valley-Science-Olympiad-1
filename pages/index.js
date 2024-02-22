d7d1d7c1b82202a5f180319b09b338e2114f2620
arushjain1030 <133838590+arushjain1030@users.noreply.github.com>
2024-02-21 20:29:30 -0800

    Fix Color on Main Page Heading

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
           content="SRVSO provides students with the chance to explore the world of STEM in a competitive setting. Science Olympiad prepares the young generation for success in future years of middle school, high school, and beyond."
         />
       </Head>
-      <h1 className="mt-16 text-l heading">
+      <h1 className="mt-16 text-white text-l heading">
         San Ramon Valley Science Olympiad
       </h1>
       <p className="mt-6 text-2xl text-center">est. 2018</p>
